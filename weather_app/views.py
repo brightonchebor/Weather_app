82c5c2a82f23eafff2f6cf5cd8579ab0f095509f
brightonchebor <cheborbrigton805@gmail.com>
2024-06-18 20:00:54 +0300

    updated

--- weather_app/views.py
+++ weather_app/views.py
@@ -12,7 +12,7 @@ def index(request):
 
     if request.method == "POST":
         city1 = request.POST['city1']
-        city2 = request.get('city2', None)
+        city2 = request.POST.get('city2', None)
 
         weather_data1, daily_forecast1 = fetch_weather_and_forecast(city1, API_KEY, current_weather_url, forecast_url)
 
